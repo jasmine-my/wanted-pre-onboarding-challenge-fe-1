1407d6e579c7942073e556f7f5bf5378aa9ff832
nae_yeonkyeong <yeonkyeong@vgen.co.kr>
2022-08-15 19:17:11 +0900

    add: React Router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import className from 'classnames/bind';
 import React from 'react';
+import { Route, Routes } from 'react-router';
+import { Link } from 'react-router-dom';
 
 import style from './App.module.scss';
 import '~/global_styles/globalFonts.scss';
 import '~/global_styles/format_styles.css';
+
 import '~/global_styles/colors.scss';
+
+import 'antd/dist/antd.css';
+import Create from '~/pages/auth/create';
 
 const cx = className.bind(style);
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img
-                    src={'/assets/images/jeju-island.jpg'}
-                    alt="sample-image"
-                />
-                <p className={cx('widget')}>테스트(widget)</p>
-                <p className={cx('callout')}>테스트(Callout1)</p>
-            </header>
+        <div>
+            <nav>
+                <Link to={'/create'}>회원가입하기</Link>
+            </nav>
+            <Routes>
+                <Route path={'/create'} element={<Create />} />
+                {/*<div className="App">*/}
+                {/*<header className="App-header">*/}
+                {/*    <img*/}
+                {/*        src={'/assets/images/jeju-island.jpg'}*/}
+                {/*        alt="sample-image"*/}
+                {/*    />*/}
+                {/*    <p className={cx('widget')}>테스트(widget)</p>*/}
+                {/*    <p className={cx('callout')}>테스트(Callout1)</p>*/}
+                {/*</header>*/}
+                {/*</div>*/}
+            </Routes>
         </div>
     );
 }
